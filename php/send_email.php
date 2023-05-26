6e40b53113dad9d3f37db9a2e1f2409c11f2e5eb
gisela <giselacriollo16@gmail.com>
2023-05-25 21:12:25 -0500

    cambios

--- php/send_email.php
+++ php/send_email.php
@@ -4,17 +4,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $email = $_POST["email"];
   $message = $_POST["message"];
 
-  // Configura la información del correo electrónico
-  $to = "giselacriollo16@gmil.com"; 
+  $to = "giselacriollo16@gmail.com"; // Reemplaza con tu dirección de correo electrónico
   $subject = "Nuevo mensaje de contacto";
   $body = "Nombre: " . $name . "\nCorreo electrónico: " . $email . "\nMensaje: " . $message;
 
-  // Envía el correo electrónico
   if (mail($to, $subject, $body)) {
-    // El correo se envió correctamente
     echo "¡Gracias por tu mensaje! Pronto nos pondremos en contacto contigo.";
   } else {
-    // Ocurrió un error al enviar el correo
     echo "Lo sentimos, hubo un error al enviar el mensaje. Por favor, inténtalo nuevamente más tarde.";
   }
 }
